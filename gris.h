d71010030990d39b27a9d1c94c946385f3b872c8
Aascente <edouard.fossatbelaud@gmail.com>
2026-04-22 14:56:48 +0200

    Update gris.h

--- gris.h
+++ gris.h
@@ -1,43 +1,19 @@
-#include "gris.h"
+#pragma once
 
+#define BLUE 0
+#define GREEN 1
+#define RED 2
 
-
-gris::gris()
+#include "filtre.h"
+class gris : public filtre
 {
-	Name = "gris";
-}
+private:
+	string Name;
+public:
+	void traitement(Mat *);
+	string getterName();
 
+	gris();
+	~gris();
+};
 
-gris::~gris()
-{
-}
-
-void gris::traitement(Mat * f) {
-
-	int lengthPicture = f->cols;
-	int heightPicture = f->rows;
-
-    size_t step = f->step;
-
-	for (int y = 0; y < heightPicture; y++) {
-		for (int x = 0; x < lengthPicture; x++) {
-            
-            int pixelIndex = y* step + x * 3;
-
-            unsigned char b = f->data[pixelIndex + BLUE];
-            unsigned char  v = f->data[pixelIndex + GREEN];
-            unsigned char  r = f->data[pixelIndex + RED];
-			
-            unsigned char moyenne = (b+v+r)/3;
-
-            f->data[pixelIndex + BLUE]  = moyenne ;
-			f->data[pixelIndex + GREEN]  = moyenne ;
-			f->data[pixelIndex + RED] = moyenne ;
-
-		}
-	}
-}
-string gris::getterName() {
-
-	return Name;
-}
